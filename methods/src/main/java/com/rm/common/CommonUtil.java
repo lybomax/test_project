1ccfe43000c5e32fe1d5d9d50e50e99e601a7b50
LiYingYing <lybomax@163.com>
2020-03-19 10:29:40 +0800

    2020年3月7日

--- methods/src/main/java/com/rm/common/CommonUtil.java
+++ methods/src/main/java/com/rm/common/CommonUtil.java
@@ -3,7 +3,9 @@ package com.rm.common;
 import com.fasterxml.uuid.Generators;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -384,5 +386,43 @@ public class CommonUtil {
 			}
 		}
 		return decimalRMB.toString();
+	}
+	
+	private Object null2String(Object obj) {
+		if (obj != null) {
+			Class<? extends Object> classz = obj.getClass();
+			// 获取所有该对象的属性值
+			Field fields[] = classz.getDeclaredFields();
+			// 遍历属性值，取得所有属性为 null 值的
+			for (Field field : fields) {
+				try {
+					Type t = field.getGenericType();
+					Type s = String.class;
+					//Type b = BigDecimal.class;
+					if (t == s) {
+						String value = BeanUtils.getProperty(obj, field.getName());
+						BeanUtils.setProperty(obj, field.getName(), StringUtils.defaultString(value, ""));
+						
+						//                        Method m = classz.getMethod("get" + change(field.getName()));
+						//                        Object name = m.invoke(obj);// 调用该字段的get方法
+						//                        if (name == null) {
+						//                            Method mtd = classz.getMethod("set" + change(field.getName()), new Class[] {String.class});// 取得所需类的方法对象
+						//                            mtd.invoke(obj, new Object[] {"  "});// 执行相应赋值方法
+						//                        }
+					}
+					//                    else if(t==b) {
+					//                        BigDecimal bg= (BigDecimal) PropertyUtils.getProperty(obj, field.getName());
+					//                        if(bg!=null) {
+					//                            BeanUtils.setProperty(obj, field.getName(), bg.toPlainString());
+					//                        }else {
+					//                            BeanUtils.setProperty(obj, field.getName(), "");
+					//                        }
+					//                    }
+				} catch (Exception e) {
+					log.error("对象属性转换异常", e);
+				}
+			}
+		}
+		return obj;
 	}
 }
